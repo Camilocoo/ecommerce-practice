0b28ba82aca1472893be83c18f3faac5dea50799
Camilo Contreras <camilo.coo250@gmail.com>
2019-05-11 20:40:07 +0000

    all components created

---
+++ ecommerce/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+export default class Navbar extends Component{
+    render(){
+        return(
+         <div>
+            <h1>Hello world Navbar</h1>
+         </div>
+        );
+    }
+}
